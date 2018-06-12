e015ab106f464e7cf9254dc9cbaef09b7e8e78c3
Aki <makitosansan@gmail.com>
2018-06-12 21:25:05 +0900

    The first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-bitdash'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
